634d682576cc45547916412a7fa19945c944201b
j13hernandez <palma@PalmBook-Pro.local>
2018-12-10 00:53:02 -0500

    Deploy to Heroku, login, sign up, sign in functions added

--- foodtasker/urls.py
+++ foodtasker/urls.py
@@ -15,7 +15,22 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from foodtaskerapp import views
+from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+    url(r'^$', views.home, name='home'),
+    url(r'^resturant/sign-in/$', auth_views.login,
+        {'template_name': 'resturant/sign_in.html'},
+        name = 'resturant-sign-in'),
+    url(r'^resturant/sign-out', auth_views.logout,
+        {'next_page': '/'},
+        name = 'resturant-sign-out'),
+    url(r'^resturant/sign-up', views.resturant_sign_up,
+        name = 'resturant-sign-up'),
+    url(r'^resturant/$', views.resturant_home,
+        name = 'resturant-home')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
